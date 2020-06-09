17c3781d468309b0e9c4f0166d2c1ef5b9810c94
alexfreed23 <alexlike23@gmail.com>
2020-06-09 20:13:58 +0300

    Conver byte in kilo/mega bytes

---
+++ Python/PythonCore/HomeWork/HomeWork_2_20200609/Task2.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Jun  9 20:07:17 2020
+
+@author: oleksiy
+"""
+
+valuiInByte = input("Enter value in byte:\n")
+
+print("Your value in kilobyte", int(valuiInByte) / 1024)
+print("Your value in megabyte", int(valuiInByte) / 1024 / 1024)
+
+
+
