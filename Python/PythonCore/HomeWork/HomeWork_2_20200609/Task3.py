c992e5707f884608ad44cc9bf24c3f51360b489d
alexfreed23 <alexlike23@gmail.com>
2020-06-09 20:19:41 +0300

    Calculate area and perimeter of the rectangle

---
+++ Python/PythonCore/HomeWork/HomeWork_2_20200609/Task3.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Jun  9 20:14:51 2020
+
+@author: oleksiy
+"""
+
+
+width = input("Enter width:\n")
+height = input("Enter height:\n")
+
+print("Area of rectangle is: ", int(width) * int(height))
+print("Perimeter of rectangle is: ", 2*(int(width) + int(height)))
+
