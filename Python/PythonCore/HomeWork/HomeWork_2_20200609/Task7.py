ba716df395bfed358bbe33a8970f2f9ec9573fd9
alexfreed23 <alexlike23@gmail.com>
2020-06-09 23:46:41 +0300

    Task 7

---
+++ Python/PythonCore/HomeWork/HomeWork_2_20200609/Task7.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Jun  9 21:10:50 2020
+
+@author: oleksiy
+"""
+# Task 7
+"""
+Different data types can use for:
+1) working with CSV files (different data in columns)
+2) JSON data (Not the best way I think)
+3) saving data about some object with differet parameters... for example User with first/last name, age, date of birth, username, password, sex, address, etc..
+
+"""
