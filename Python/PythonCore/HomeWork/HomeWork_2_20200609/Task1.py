f3f369a8717caebb63aa39ce18c76d9b9f181d99
alexfreed23 <alexlike23@gmail.com>
2020-06-09 20:05:56 +0300

    Swapping values of two variables

---
+++ Python/PythonCore/HomeWork/HomeWork_2_20200609/Task1.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Jun  9 19:49:29 2020
+
+@author: oleksiy
+"""
+
+#Task1
+
+a = input("Enter first value:\n")
+b = input("Enter second value:\n")
+
+a,b = b,a
+
+print("a: ", a)
+print("b: ", b)
+
