a914e7611ee8307fbabbf69ba7ab7a0b9406c73d
alexfreed23 <alexlike23@gmail.com>
2020-06-13 23:06:53 +0300

    Added try - catch block and checking values less or equal 0

--- Python/PythonCore/HomeWork/HomeWork_3_20200612/Task5.py
+++ Python/PythonCore/HomeWork/HomeWork_3_20200612/Task5.py
@@ -8,16 +8,20 @@ Created on Sat Jun 13 19:31:27 2020
 without checking if user enter letters
 
 """
+try:
+    money = int(input("Enter value of UAN to exchange:"))
 
-money = int(input("Enter value of UAN to exchange:"))
-
-if money % 200 == 0:
-    print("To exchange ", money, " needed ", money // 200, " banknote(s) of 200 UAH")
-elif money % 200 != 0 and money % 100 == 0:
-    print("To exchange ", money, " needed ", money // 200, "banknote(s) of 200 UAH \
+    if money <= 0:
+        print("Unable to exchange", money," UAH")
+    elif money % 200 == 0:
+        print("To exchange ", money, " needed ", money // 200, " banknote(s) of 200 UAH")
+    elif money % 200 != 0 and money % 100 == 0:
+        print("To exchange ", money, " needed ", money // 200, "banknote(s) of 200 UAH \
 and ", (money % 200) // 100, " 100 UAH banknote(s)")
-elif money % 200 != 0 and money % 100 != 0 and money % 10 == 0:
-    print("To exchange ", money, " needed ", money // 200, "banknote(s) of 200 UAH  \
+    elif money % 200 != 0 and money % 100 != 0 and money % 10 == 0:
+        print("To exchange ", money, " needed ", money // 200, "banknote(s) of 200 UAH  \
 and ", (money % 200) // 100, " 100 UAH banknote(s) and ", ((money % 200) % 100) // 10, "banknote(s) of 10 UAN")
-elif money % 200 != 0 and money % 100 != 0 and money % 10 != 0:
-    print("To exchange ", money, " needed \n", money // 200, "banknote(s) of 200 UAH and \n", (money % 200) // 100, "banknote(s) of 100 UAH and \n", ((money % 200) % 100) // 10, "banknote(s) of 10 UAN and \n", ((money % 200) % 100) % 10, "banknote(s) of 1 UAH")
+    elif money % 200 != 0 and money % 100 != 0 and money % 10 != 0:
+        print("To exchange ", money, " needed \n", money // 200, "banknote(s) of 200 UAH and \n", (money % 200) // 100, "banknote(s) of 100 UAH and \n", ((money % 200) % 100) // 10, "banknote(s) of 10 UAN and \n", ((money % 200) % 100) % 10, "banknote(s) of 1 UAH")
+except ValueError:
+    print("That's not a number")
