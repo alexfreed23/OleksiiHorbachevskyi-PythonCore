d9023b9497abdee63b7968b3f97a09b91df337f7
alexfreed23 <alexlike23@gmail.com>
2020-06-14 21:13:11 +0300

    deleted unnecessary if blocks

--- Python/PythonCore/HomeWork/HomeWork_3_20200612/Task2.py
+++ Python/PythonCore/HomeWork/HomeWork_3_20200612/Task2.py
@@ -9,17 +9,16 @@ import math
 
 figure = input("Please select figure and enter respective number:\n 1 - rectangle\n 2 - triangle\n 3 - circle\n")
 
-if int(figure) == 1 or int(figure) == 2:
-    if int(figure) == 1:
-        width = float(input("Enter width:\n"))
-        hight = float(input("Enter hight:\n"))
-        print("The square of rectangle is: ", width*hight)
-    else:
-        side1 = float(input("Enter side 1:\n"))
-        side2 = float(input("Enter side 2:\n"))
-        side3 = float(input("Enter side 3:\n"))
-        p = (side1 + side2 + side3)/2
-        print("The square of triangle is: ",math.sqrt(p*(p - side1)*(p - side2)*(p - side3)))
+if int(figure) == 1:
+    width = float(input("Enter width:\n"))
+    hight = float(input("Enter hight:\n"))
+    print("The square of rectangle is: ", width*hight)
+elif int(figure) == 2:
+    side1 = float(input("Enter side 1:\n"))
+    side2 = float(input("Enter side 2:\n"))
+    side3 = float(input("Enter side 3:\n"))
+    p = (side1 + side2 + side3)/2
+    print("The square of triangle is: ",math.sqrt(p*(p - side1)*(p - side2)*(p - side3)))
 elif int(figure) == 3:
     radius = float(input("Enter radius:\n"))
     print("The square of circle is: ", math.pi * radius**2)
