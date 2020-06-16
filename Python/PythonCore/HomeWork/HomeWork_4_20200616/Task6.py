32162fcebd7e60f9a5480a256f1b257d7c45575b
alexfreed23 <alexlike23@gmail.com>
2020-06-16 22:26:25 +0300

    check count of positive and negative numbers in percents

---
+++ Python/PythonCore/HomeWork/HomeWork_4_20200616/Task6.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Jun 16 21:05:31 2020
+
+@author: oleksiy
+"""
+
+noNneedExit = True
+positive = 0
+negative = 0
+
+while noNneedExit:
+    enteredNumber = int(input("Enter number: "))
+    if enteredNumber == 0:
+        noNneedExit = False
+        continue
+    if enteredNumber < 0:
+        negative += 1
+    if enteredNumber > 0:
+        positive += 1
+print("Entered positive numbers in percents: ", round((positive / (positive + negative)) * 100), "%")
+print("Entered negative numbers in percents: ", round((negative / (positive + negative)) * 100), "%")
