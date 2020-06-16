0e34e5c65bdf3b1600c892294ade137a53c14f21
alexfreed23 <alexlike23@gmail.com>
2020-06-16 20:02:17 +0300

    Task 1

---
+++ Python/PythonCore/HomeWork/HomeWork_4_20200616/Task1.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Jun 16 19:42:45 2020
+
+@author: oleksiy
+"""
+
+import random
+
+secretNumber = random.randint(0, 100)
+tryNumber = 1
+while tryNumber <=10:
+    userNumber = int(input("Enter your number: "))
+    if userNumber < secretNumber:
+        print("Your number is less than SECRET NUMBER")
+    elif userNumber > secretNumber:
+        print("Your number is more than SECRET NUMBER")
+    else:
+        print("Absolutely RIGHT!!!")
+        break
+print("Secret number is: ", secretNumber)
