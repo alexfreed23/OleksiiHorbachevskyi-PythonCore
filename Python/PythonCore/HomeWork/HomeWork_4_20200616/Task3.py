0a6da0d1d7da3fda2c56f1a733d49447f195a7da
alexfreed23 <alexlike23@gmail.com>
2020-06-16 20:36:58 +0300

    Task3 - aka Pifagor

---
+++ Python/PythonCore/HomeWork/HomeWork_4_20200616/Task3.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Jun 16 20:34:03 2020
+
+@author: oleksiy
+"""
+
+
+for i in range(1,10):
+    for j in range(1,10):
+        print(i,"x", j, "=", i*j)
+    print()
+    
