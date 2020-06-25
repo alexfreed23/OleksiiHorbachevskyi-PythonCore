ed946b12f1551b501f86a1f9ee2a423fca8bea45
alexfreed23 <alexlike23@gmail.com>
2020-06-25 20:21:22 +0300

    Task 5 - relpace spaces by underscores

--- Python/PythonCore/HomeWork/HomeWork_5_20200619/Task5.py
+++ Python/PythonCore/HomeWork/HomeWork_5_20200619/Task5.py
@@ -7,22 +7,11 @@ Created on Thu Jun 25 19:19:00 2020
 """
 
 enteredText = input("Enter text: ")
-
-isUpperCase = False
-isLowerCase = False
-
+resultText = ""
 for letter in enteredText:
-    if ord(letter) in range(97,123):
-        isLowerCase = True
-    if ord(letter) in range(65,91):
-        isUpperCase = True
-if isUpperCase:
-    print("Text contain UPPERCASE letter")
-else:
-    print("Text does not contain UPPERCASE letter")
-    
-if isLowerCase:
-    print("Text contain lowercase letter")
-else:
-    print("Text does not contain lowercase letter")
+    if ord(letter) == 32:
+        resultText = resultText + "_"
+    else:
+        resultText = resultText + letter
+print(resultText)
     
