3866c52500c7c9afd4d23b471ea29376f08289c4
alexfreed23 <alexlike23@gmail.com>
2020-06-25 19:12:11 +0300

    Task 4 - count spaces

---
+++ Python/PythonCore/HomeWork/HomeWork_5_20200619/Task4.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Jun 25 19:06:27 2020
+
+@author: oleksiy
+"""
+
+enteredString = input("Enter string:")
+spacesCount = 0
+
+for element in enteredString:
+    if ord(element) == 32:
+        spacesCount += 1
+print("In entered string ", spacesCount, " spaces")
