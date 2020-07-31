39922606fc899a421c0e47d20793b40cda02c2d2
alexfreed23 <alexlike23@gmail.com>
2020-07-31 19:38:08 +0300

    Task 1 - Calculator with module

---
+++ Python/PythonCore/HomeWork/HomeWork_20200731/calculator/calculator.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Jul 31 19:29:26 2020
+
+@author: oleksii
+"""
+
+def summ(a,b):
+    return a + b
+def diff(a,b):
+    return a - b
+def product(a,b):
+    return a *b
+def fraction(a,b):
+    return a / b
+
---
+++ Python/PythonCore/HomeWork/HomeWork_20200731/calculator/task1_calc.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Jul 31 19:33:07 2020
+
+@author: oleksii
+"""
+
+#from calculator import *
+from calculator import summ,diff,product,fraction
+
+print(summ(5,5))
+print(diff(5,5))
+print(product(5,5))
+print(fraction(5,5))
+
