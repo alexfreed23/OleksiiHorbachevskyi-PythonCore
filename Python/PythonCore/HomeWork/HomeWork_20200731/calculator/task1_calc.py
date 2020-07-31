d329f1bc0e15f0e9eec8951018c2f6078da32607
alexfreed23 <alexlike23@gmail.com>
2020-07-31 19:43:25 +0300

    updated as in task

--- Python/PythonCore/HomeWork/HomeWork_20200731/calculator/task1_calc.py
+++ Python/PythonCore/HomeWork/HomeWork_20200731/calculator/task1_calc.py
@@ -6,11 +6,26 @@ Created on Fri Jul 31 19:33:07 2020
 @author: oleksii
 """
 
-#from calculator import *
+import calculator
+
+print(calculator.summ(10,5))
+print(calculator.diff(30,5))
+print(calculator.product(2,5))
+print(calculator.fraction(25,5))
+print("-"*10)
+
+from calculator import summ,diff
+print(summ(5,5))
+print(diff(5,5))
+print("-"*10)
+
+
 from calculator import summ,diff,product,fraction
+# or
+#from calculator import *
 
 print(summ(5,5))
 print(diff(5,5))
 print(product(5,5))
 print(fraction(5,5))
-
+print("-"*10)
